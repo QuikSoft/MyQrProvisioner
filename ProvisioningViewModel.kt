0aca0e2a4ec60dce386ed08de5e48626d61e9819
Jeroen J.V Lebon <jjvlebon@ziggo.nl>
2025-07-04 14:51:32 +0200

    Create ProvisioningViewModel.kt

---
+++ ProvisioningViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.qrprovisioner
+import android.graphics.Bitmap
+import android.graphics.Color
+import androidx.lifecycle.ViewModel
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.qrcode.QRCodeWriter
+import kotlinx.coroutines.flow.*
+import java.util.*
+
+class ProvisioningViewModel : ViewModel() {
+    // Same content as provided earlier...
+}
